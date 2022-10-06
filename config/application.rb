dbcf1818538afb3b570b6241bace00e9c4105511
Justin Coyne <jcoyne@justincoyne.com>
2022-10-06 09:20:52 -0500

    Add rubocop and rubocop-rails

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "boot"
 
 require "rails/all"
@@ -7,6 +9,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module NtaArclight
+  # Holds configuration for the application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
