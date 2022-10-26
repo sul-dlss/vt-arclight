5078b10a36ca359a74b8fec40dad9c0a033f51b9
Justin Coyne <jcoyne@justincoyne.com>
2022-10-26 15:58:37 -0500

    Remove the group toggle
    Fixes #78

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -46,7 +46,6 @@ class CatalogController < ApplicationController # rubocop:disable Metrics/ClassL
 
     config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
 
-    config.add_results_collection_tool(:group_toggle)
     config.add_results_collection_tool(:sort_widget)
     config.add_results_collection_tool(:per_page_widget)
     config.add_results_collection_tool(:view_type_group)
