cc0794a8a7eb74686a31327412a257b20a1907e9
Justin Coyne <jcoyne@justincoyne.com>
2022-10-26 14:01:53 -0500

    Don't show Home or repository link in breadcrumbs
    Fixes #85

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -70,6 +70,7 @@ class CatalogController < ApplicationController # rubocop:disable Metrics/ClassL
     # solr field configuration for document/show views
     # config.show.title_field = 'title_display'
     config.show.document_component = Arclight::DocumentComponent
+    config.show.breadcrumb_component = BreadcrumbHierarchyComponent
     config.show.embed_component = Arclight::EmbedComponent
     config.show.display_type_field = 'level_ssm'
     # config.show.thumbnail_field = 'thumbnail_path_ss'
