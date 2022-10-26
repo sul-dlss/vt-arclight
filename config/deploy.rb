3755dfe166a3551332ac1dc8c07ee468ae313193
Nick Budak <budak@stanford.edu>
2022-10-26 10:56:27 -0700

    Update branch name to main for continuous deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,12 @@
 set :application, "nta-arclight"
 set :repo_url, "https://github.com/sul-dlss/nta-arclight.git"
 
-# Default branch is :master
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call unless ENV['DEPLOY']
+# Default branch is :master so we need to update to main
+if ENV['DEPLOY']
+  set :branch, 'main'
+else
+  ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+end
 
 # Default deploy_to directory is /var/www/application-name
 set :deploy_to, "/opt/app/nta/nta"
