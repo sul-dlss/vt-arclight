a4a1ca0da1b022f5d6c2084e8882bbd3d62cdb2d
Marlo Longley <mlongley@stanford.edu>
2022-11-17 14:29:26 -0500

    Increase HTTP timeout for SolrWriter

--- lib/traject/vt_config.rb
+++ lib/traject/vt_config.rb
@@ -4,6 +4,7 @@ require 'arclight'
 
 settings do
   provide 'component_traject_config', File.join(__dir__, 'vt_component_config.rb')
+  provide 'solr_writer.http_timeout', 1200
 end
 
 load_config_file(File.expand_path("#{Arclight::Engine.root}/lib/arclight/traject/ead2_config.rb"))
