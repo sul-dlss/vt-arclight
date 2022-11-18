a362f2a034af4006f0150ee2eb5934eedf6fe7d9
Nick Budak <budak@stanford.edu>
2022-11-18 14:46:01 -0800

    Allow converting a single file to xlsx

--- source_data/csv2xlsx.py
+++ source_data/csv2xlsx.py
@@ -5,13 +5,22 @@ import sys
 import pandas as pd
 
 # get the path to the directory containing the CSV files
-input_dir = sys.argv[1]
+input_path = pathlib.Path(sys.argv[1])
 
-for csv_file in pathlib.Path(input_dir).glob("*.csv"):
+
+def convert_file(file: pathlib.Path):
+    """Convert a single CSV file to an XLSX file."""
     # read the CSV file into a pandas DataFrame
-    df = pd.read_csv(csv_file, dtype="str")
+    df = pd.read_csv(file, dtype="str")
 
     # create an XLSX file from the DataFrame
-    xlsx_file = csv_file.with_suffix(".xlsx")
+    xlsx_file = file.with_suffix(".xlsx")
     df.to_excel(xlsx_file, index=False)
     print(xlsx_file)
+
+
+if input_path.is_dir():
+    for csv_file in input_path.glob("*.csv"):
+        convert_file(csv_file)
+else:
+    convert_file(input_path)
