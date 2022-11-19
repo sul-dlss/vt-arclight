d544c0c2114350728ea7c302c2308e6654a20bc1
Nick Budak <budak@stanford.edu>
2022-11-18 17:41:27 -0800

    Use openpyxl directly for xlsx generation

--- source_data/csv2xlsx.py
+++ source_data/csv2xlsx.py
@@ -2,7 +2,8 @@
 
 import pathlib
 import sys
-import pandas as pd
+import openpyxl
+import csv
 
 # get the path to the directory containing the CSV files
 input_path = pathlib.Path(sys.argv[1])
@@ -10,13 +11,21 @@ input_path = pathlib.Path(sys.argv[1])
 
 def convert_file(file: pathlib.Path):
     """Convert a single CSV file to an XLSX file."""
-    # read the CSV file into a pandas DataFrame
-    df = pd.read_csv(file, dtype="str")
+    # read the CSV file into a list of rows
+    with file.open("r", encoding="utf-8") as infile:
+        reader = csv.reader(infile)
+        data_rows = [row for row in reader]
 
-    # create an XLSX file from the DataFrame
-    xlsx_file = file.with_suffix(".xlsx")
-    df.to_excel(xlsx_file, index=False)
-    print(xlsx_file)
+    # create a new workbook and add a worksheet
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    # write the data to the sheet and save it
+    for row in data_rows:
+        ws.append(row)
+    output_file = file.parent / (file.stem + ".xlsx")
+    wb.save(output_file)
+    print(output_file)
 
 
 if input_path.is_dir():
