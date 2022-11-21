6f76e7175780f001d687ec555dd5d6750a452b16
Nick Budak <budak@stanford.edu>
2022-11-21 14:38:36 -0800

    Uncomment traject config block

--- lib/traject/vt_component_config.rb
+++ lib/traject/vt_component_config.rb
@@ -10,32 +10,25 @@ end
 load_config_file(File.expand_path("#{Arclight::Engine.root}/lib/arclight/traject/ead2_component_config.rb"))
 
 # FULL TEXT FIELDS
-# to_field 'full_text_tesimv' do |resource, accumulator, _context|
-#   druid = resource.xpath(".//dao/@href").map(&:value).map do |value|
-#     value.delete_prefix("https://purl.stanford.edu/")
-#   end.first
-#   next unless druid
+to_field 'full_text_tesimv' do |resource, accumulator, _context|
+  druid = resource.xpath(".//dao/@href").map(&:value).map do |value|
+    value.delete_prefix("https://purl.stanford.edu/")
+  end.first
+  next unless druid
 
-#   filename = "#{settings['fulltext_data_dir']}/#{druid}.txt"
-#   if File.exist?(filename)
-#     accumulator << File.read(filename)
-#   else
-#     logger.warn("Missing fulltext source: #{filename}")
-#   end
-# end
+  filename = "#{settings['fulltext_data_dir']}/#{druid}.txt"
+  if File.exist?(filename)
+    accumulator << File.read(filename)
+  else
+    logger.warn("Missing fulltext source: #{filename}")
+  end
+end
 
 to_field 'language_ssim' do |resource, accumulator, _context|
   raw = resource.xpath('./controlaccess/function[@source="lcsh"]')
   raw.map(&:text).map { |elem| elem.delete_suffix(' language') }.each do |val|
     accumulator << val
   end
-end
-
-to_field 'resource_type_ssim' do |resource, accumulator, context|
-  level = context.output_hash['level_ssm'].first
-  next unless level == 'Item'
-
-  accumulator << resource.xpath('ancestor::c[@level="series"]/did/unittitle').text
 end
 
 to_field 'resource_format_ssim', extract_xpath('./did/physdesc/physfacet')
