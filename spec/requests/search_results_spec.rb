a0b5d5729cad14deab62d6bab4ac1af1177b5410
Justin Coyne <jcoyne@justincoyne.com>
2022-11-23 13:31:05 -0600

    Move facet labels to translations

--- spec/requests/search_results_spec.rb
+++ spec/requests/search_results_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "The search result page" do
 
   context 'when a facet is selected' do
     it "has a single start over link" do
-      get "/nuremberg?f[date_range_sim][]=1945&q=&search_field=all_fields"
+      get "/nuremberg?f[date_range][]=1945&q=&search_field=all_fields"
 
       expect(response).to have_http_status(:ok)
       expect(page).to have_link 'Start Over', count: 1
