1205a3c8e90641d094787c374559d33dac82473c
Justin Coyne <jcoyne@justincoyne.com>
2022-11-28 10:49:57 -0600

    Add a item level content warning
    Fixes #232

--- app/components/embed_component.rb
+++ app/components/embed_component.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
-# Overrides Arclight to removes the title header
+# Overrides Arclight to removes the title header and add a content warning
 class EmbedComponent < Arclight::EmbedComponent
+  VIDEO_TYPE = 'Moving Images'
+  IMAGE_TYPE = 'Graphic Materials'
+
+  def content_warning
+    return unless video? || image?
+
+    tag.div t('.content_warning_html'), class: "alert global-warning"
+  end
+
+  def video?
+    media_format == VIDEO_TYPE
+  end
+
+  def image?
+    media_format == IMAGE_TYPE
+  end
+
+  delegate :media_format, to: :@document
 end
