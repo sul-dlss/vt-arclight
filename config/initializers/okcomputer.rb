cbf1c65a4373c3f5c8834b05bc4f58640e335019
Justin Coyne <jcoyne@justincoyne.com>
2022-11-30 10:00:54 -0600

    Add okcomputer to report application status

---
+++ config/initializers/okcomputer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# # /status for 'upness', e.g. for load balancer
+# # /status/all to show all dependencies
+# # /status/<name-of-check> for a specific check (e.g. for nagios warning)
+OkComputer.mount_at = 'status'
