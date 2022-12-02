e4083c67d99eb5dfb2c6e5dfbec4293ce7d41f6e
Marlo Longley <mlongley@stanford.edu>
2022-12-02 17:53:19 -0500

    Add new text to VT landing page

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe "The home page" do
     expect(page).to have_text "Virtual Tribunals at Stanford"
 
     # Main
-    expect(page).to have_text 'Virtual Tribunals is a collaboration'
-    expect(page).to have_text 'Archival Collection'
-    expect(page).to have_text 'Virtual Tribunals Online Exhibit'
+    expect(page).to have_text 'Virtual Tribunals is a major initiative'
+    expect(page).to have_text 'Taube Archive of the International Military Tribunal (IMT) at Nuremberg, 1945-46'
+    expect(page).to have_text 'Virtual Tribunals Spotlight exhibit'
   end
 end
