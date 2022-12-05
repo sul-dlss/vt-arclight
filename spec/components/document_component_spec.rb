cb8946001e02b223a4f7ce0dffb847dacb1477f1
Marlo Longley <mlongley@stanford.edu>
2022-12-05 17:14:56 -0500

    Make document spec more realistic and add media format test

--- spec/components/document_component_spec.rb
+++ spec/components/document_component_spec.rb
@@ -22,15 +22,16 @@ RSpec.describe DocumentComponent, type: :component do
 
   context 'with a component with metadata' do
     let(:document) do
-      SolrDocument.new(level_ssm: ['Series'],
+      SolrDocument.new(level_ssm: ['Item'],
                        normalized_date_ssm: ['1946'],
                        scopecontent_ssm: ["<p>The collection in the Virtual Tribunal platform contains...</p>",
                                           "<p>The official archives of the International Military Tribunal...</p>"],
-                       containers_ssim: ['box 5'],
+                       containers_ssim: ['Box 25'],
                        language_ssim: ['German'],
                        resource_type_ssim: ['Exhibits'],
-                       resource_format_ssim: ['Loose leaf folders'],
-                       unitid_ssm: ['H-2123'])
+                       resource_format_ssim: ['Photo albums'],
+                       unitid_ssm: ['H-5006'],
+                       media_format_ssi: ['Graphic Materials'])
     end
 
     it "shows the data" do
@@ -39,22 +40,26 @@ RSpec.describe DocumentComponent, type: :component do
       expect(page).to have_text 'The official archives of the International'
 
       expect(page).to have_text 'Container:'
-      expect(page).to have_text 'Box 5'
+      expect(page).to have_text 'Box 25'
 
       expect(page).to have_text 'Document number:'
-      expect(page).to have_text 'H-2123'
+      expect(page).to have_text 'H-5006'
 
       expect(page).to have_text 'Date:'
       expect(page).to have_text '1946'
 
       expect(page).to have_text 'Resource format:'
-      expect(page).to have_text 'Loose leaf folders'
+      expect(page).to have_text 'Photo albums'
 
       expect(page).to have_text 'Resource type:'
       expect(page).to have_text 'Exhibits'
 
       expect(page).to have_text 'Language:'
       expect(page).to have_text 'German'
+
+      # Label will be modified from the "Graphic Materials" above by the :media_format_label helper
+      expect(page).to have_text 'Media format'
+      expect(page).to have_text 'Image'
     end
   end
 end
