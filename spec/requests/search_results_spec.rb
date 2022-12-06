8600d42b3c701e849d043d99884e563243592fb3
Justin Coyne <jcoyne@justincoyne.com>
2022-12-06 09:52:07 -0600

    Allow searching for a date
    Fixes #353

--- spec/requests/search_results_spec.rb
+++ spec/requests/search_results_spec.rb
@@ -15,4 +15,33 @@ RSpec.describe "The search result page" do
       expect(page).to have_link 'Start Over', count: 1
     end
   end
+
+  context 'when searching for a year' do
+    let(:blacklight_config) { CatalogController.blacklight_config }
+    let(:solr_conn) { blacklight_config.repository_class.new(blacklight_config).connection }
+    let(:id) { 'mt839rq8746aspace_b2d40ef7acf229edec643750a8240999' }
+
+    before do
+      solr_conn.add({
+                      id:,
+                      normalized_title_ssm: ['Document books'],
+                      level_ssm: ['Series'],
+                      collection_sim: ['some collection'],
+                      parent_ssim: %w[def ghi],
+                      date_hierarchy_ssim: %w[1938 1938-10 1938-10-12 1938-10-13 1938-10-14],
+                      ead_ssi: 'abc123',
+                      repository_ssm: 'my repository',
+                      scopecontent_ssm: ["<p>The collection in the Virtual Tribunal platform contains...</p>",
+                                         "<p>The official archives of the International Military Tribunal...</p>"]
+                    })
+      solr_conn.commit
+    end
+
+    it "returns a result" do
+      get "/nuremberg?q=1938&search_field=all_fields"
+
+      expect(response).to have_http_status(:ok)
+      expect(page).to have_link 'Document books'
+    end
+  end
 end
