1e9f450218f89fa2f3fe0ec5654183595a643168
Justin Coyne <jcoyne@justincoyne.com>
2022-12-06 13:41:56 -0600

    Add a recaptcha to the feedback form

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,7 +3,12 @@
 # Handles submission of the feedback form
 class FeedbacksController < ApplicationController
   def create
-    FeedbackMailer.submit_feedback(name: params[:name], email: params[:email], message: params[:message]).deliver_now
-    redirect_to root_path, notice: t(".success")
+    if verify_recaptcha(action: 'feedback')
+      FeedbackMailer.submit_feedback(name: params[:name], email: params[:email], message: params[:message]).deliver_now
+      redirect_to root_path, notice: t(".success")
+    else
+      # Score is below threshold, so user may be a bot.
+      redirect_to root_path, alert: t(".failure")
+    end
   end
 end
