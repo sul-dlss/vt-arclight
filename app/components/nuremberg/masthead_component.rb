e5c7b2bcc7812d287a41bb66536fc3a5a8d5e6e3
Chris Beer <chris@cbeer.info>
2022-12-15 13:23:34 -0800

    Prevent orphans when wrapping the masthead title

--- app/components/nuremberg/masthead_component.rb
+++ app/components/nuremberg/masthead_component.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module Nuremberg
+  # Overrides the default Arclight::MastheadComponent with our
+  # own design and html-safe'd heading
   class MastheadComponent < Arclight::MastheadComponent
+    def heading
+      sanitize super
+    end
   end
 end
