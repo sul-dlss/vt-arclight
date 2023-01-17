9027dcf1ef1a53cadb4a9d411be687dc133ba97f
Marlo Longley <mlongley@stanford.edu>
2023-01-17 14:59:59 -0500

    Fix broken debug_mode for Google Analytics

--- app/javascript/controllers/analytics_controller.js
+++ app/javascript/controllers/analytics_controller.js
@@ -2,10 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    const debug = Boolean(document.head.querySelector("meta[name=analytics_debug]").getAttribute("value"))
+    // We pass an integer ENV value through this meta tag to get it into the controller here
+    // In the process the value is transformed into a String
+    const debug = document.head.querySelector("meta[name=analytics_debug]").getAttribute("value") === "1"
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-48Y3Q1L4K0', {'debug_mode': debug});
+    // To turn off debug mode, exclude the parameter altogether (cannot just set to false)
+    // See https://support.google.com/analytics/answer/7201382?hl=en#zippy=%2Cgoogle-tag-websites
+    const args = {}
+    if (debug) {
+      args["debug_mode"] = debug
+    }
+    gtag('config', 'G-48Y3Q1L4K0', args)
   }
 }
