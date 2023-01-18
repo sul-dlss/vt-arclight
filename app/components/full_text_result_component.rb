8cc16321a756bf8e7c4c76469e373478917805bf
Nick Budak <budak@stanford.edu>
2023-01-18 12:35:30 -0800

    Show a link to search within documents on the catalog
    Ref #155

--- app/components/full_text_result_component.rb
+++ app/components/full_text_result_component.rb
@@ -2,6 +2,15 @@
 
 # Render the fulltext results
 class FullTextResultComponent < Blacklight::MetadataFieldComponent
+  attr_reader :query_param
+
+  def initialize(field:, **)
+    super
+
+    # Save the search so we can pass it to the show page for searching the document
+    @query_param = field.view_context.search_state.query_param
+  end
+
   def key
     "#{@field.key}-#{@field.document.id}"
   end
--- spec/components/full_text_result_component_spec.rb
+++ spec/components/full_text_result_component_spec.rb
@@ -1,15 +1,32 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe FullTextResultComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:view_context) { controller.view_context }
+  let(:field_config) { Blacklight::Configuration::Field.new(key: 'ftkey', field: 'fulltext', label: 'Full Text') }
+  let(:document) { SolrDocument.new(id: 123, fulltext: ['my text']) }
+  let(:field) { Blacklight::FieldPresenter.new(view_context, document, field_config) }
+  let(:params) { {} }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  before do
+    allow(view_context).to receive(:search_state).and_return(Blacklight::SearchState.new(params, nil))
+    render_inline(described_class.new(field:))
+  end
+
+  it 'renders the full text content' do
+    expect(page).to have_selector '#ftkey-123', text: 'my text'
+  end
+
+  it 'renders a button to expand the full text preview' do
+    expect(page).to have_button 'expand'
+  end
+
+  context 'with a search active' do
+    let(:params) { { q: 'mytext' } }
+
+    it 'renders a link to search within the document' do
+      expect(page).to have_link 'Search for "mytext" in document text', href: '/catalog/123?q=mytext'
+    end
+  end
 end
