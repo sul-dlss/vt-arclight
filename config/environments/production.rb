09540d43bb4ce69cfa1a9124a11cacdc8a5a082f
Justin Coyne <jcoyne@justincoyne.com>
2023-02-21 09:58:37 -0600

    Add reconnect attempts for the redis cache
    This will be the default setting in Rails 7.1.
    Fixes #532

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use redis as the cache in production.
-  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL') }
+  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL'), reconnect_attempts: 2 }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
