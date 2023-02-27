2d2d2d061df95aeb205f39572cb7657489f8121f
Nick Budak <budak@stanford.edu>
2023-02-27 14:52:00 -0800

    Subclass OembedController to pass extra params to sul-embed
    This restores support for the "search within document" feature.
    
    Also hides the viewer title, since it just duplicates the <h1> on
    the page.

--- spec/components/oembed_viewer_component_spec.rb
+++ spec/components/oembed_viewer_component_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe OembedViewerComponent, type: :component do
   end
 
   it 'renders the viewer' do
-    expect(page).to have_selector 'div[data-controller="arclight-oembed"]'
+    expect(page).to have_selector 'div[data-controller="sul-embed"]'
   end
 
   it 'renders a link to the resource' do
@@ -21,10 +21,10 @@ RSpec.describe OembedViewerComponent, type: :component do
   end
 
   it 'passes the oembed URL to the viewer' do
-    expect(page).to have_selector 'div[data-arclight-oembed-url-value="example.com"]'
+    expect(page).to have_selector 'div[data-sul-embed-url-value="example.com"]'
   end
 
   it 'passes the search term to the viewer' do
-    expect(page).to have_selector 'div[data-arclight-oembed-search="mytext"]'
+    expect(page).to have_selector 'div[data-sul-embed-search-value="mytext"]'
   end
 end
