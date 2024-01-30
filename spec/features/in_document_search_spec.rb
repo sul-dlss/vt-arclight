94d69153f15e877ef12a9ec66217f233a0de6ac4
Steve Taylor <sctaylor@stanford.edu>
2024-01-30 15:32:55 -0800

    Check if it is extremely slow

--- spec/features/in_document_search_spec.rb
+++ spec/features/in_document_search_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe "Highlighted search terms in viewer", js: true do
       page.find('.mirador-companion-area-left')
 
       # input is populated with the search term
-      expect(page).to have_selector('input[value="justice"]', wait: 10)
+      page.find('input[value="justice"]', wait: 60)
 
       # buttons are present
       expect(page.find('h3')).to have_text 'Search'
