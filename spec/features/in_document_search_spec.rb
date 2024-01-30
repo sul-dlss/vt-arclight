8bbd878f0b61f1c2404452e12ec68d493f88bd62
Steve Taylor <sctaylor@stanford.edu>
2024-01-30 15:35:57 -0800

    Reduce the wait time

--- spec/features/in_document_search_spec.rb
+++ spec/features/in_document_search_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe "Highlighted search terms in viewer", js: true do
       page.find('.mirador-companion-area-left')
 
       # input is populated with the search term
-      page.find('input[value="justice"]', wait: 60)
+      page.find('input[value="justice"]', wait: 30)
 
       # buttons are present
       expect(page.find('h3')).to have_text 'Search'
