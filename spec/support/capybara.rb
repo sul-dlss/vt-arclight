922f1deb023c7c123952b52f56f601bd79b9e996
Steve Taylor <sctaylor@stanford.edu>
2024-01-31 06:54:31 -0800

    Test max wait time again

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-Capybara.register_driver :selenium_chrome_headless_old do |app|
-  browser_options = Selenium::WebDriver::Chrome::Options.new
-  browser_options.add_argument('--headless=old')
-  browser_options.add_argument('--disable-site-isolation-trials')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-end
+# Capybara.register_driver :selenium_chrome_headless_old do |app|
+#   browser_options = Selenium::WebDriver::Chrome::Options.new
+#   browser_options.add_argument('--headless=old')
+#   browser_options.add_argument('--disable-site-isolation-trials')
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+# end
 
-Capybara.javascript_driver = :selenium_chrome_headless_old
-# Capybara.javascript_driver = :selenium_chrome_headless
+# Capybara.javascript_driver = :selenium_chrome_headless_old
+Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.default_max_wait_time = 60
