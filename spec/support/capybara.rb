6b5b6b54c0c34e25803151beedf642585a7e535a
Steve Taylor <sctaylor@stanford.edu>
2024-01-31 07:09:33 -0800

    Is this still flappy at 30

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -9,5 +9,4 @@
 
 # Capybara.javascript_driver = :selenium_chrome_headless_old
 Capybara.javascript_driver = :selenium_chrome_headless
-Capybara.default_max_wait_time = ENV['CI'] ? 60 : 10
-puts "Capybara.default_max_wait_time = #{Capybara.default_max_wait_time}"
+Capybara.default_max_wait_time = ENV['CI'] ? 30 : 10
