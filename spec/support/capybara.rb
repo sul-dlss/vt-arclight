fa38dc440f96583dfa96d8049dbafe2a80befce5
Steve Taylor <sctaylor@stanford.edu>
2024-01-30 19:25:55 -0800

    Checking how flappy test is on old

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-# Capybara.register_driver :selenium_chrome_headless_old do |app|
-#   browser_options = Selenium::WebDriver::Chrome::Options.new
-#   browser_options.add_argument('--headless=old')
-#   browser_options.add_argument('--disable-site-isolation-trials')
-#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-# end
+Capybara.register_driver :selenium_chrome_headless_old do |app|
+  browser_options = Selenium::WebDriver::Chrome::Options.new
+  browser_options.add_argument('--headless=old')
+  browser_options.add_argument('--disable-site-isolation-trials')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
 
-# Capybara.javascript_driver = :selenium_chrome_headless_old
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :selenium_chrome_headless_old
+# Capybara.javascript_driver = :selenium_chrome_headless
