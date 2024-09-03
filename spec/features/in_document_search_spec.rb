e22e26c31381d18d6ef1e1c2f631717f18a48feb
Steve Taylor <sctaylor@stanford.edu>
2024-09-03 08:58:33 -0700

    Trying to figure CI tests out

--- spec/features/in_document_search_spec.rb
+++ spec/features/in_document_search_spec.rb
@@ -37,6 +37,7 @@ RSpec.describe "Highlighted search terms in viewer", :js do
       # sidebar is open
       page.find('.mirador-companion-area-left')
       # input is populated with the search term
+      # Checking if this passes normally.
       page.find('input[value="justice"]')
       # buttons are present
       expect(page.find('h3')).to have_text 'Search'
