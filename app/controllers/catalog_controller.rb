d1ac7ea344fcbe96538cea8a0a7f1270edf2c731
Chris Beer <cabeer@stanford.edu>
2026-06-02 14:19:11 -0700

    Update to Rails 8.1

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -5,15 +5,7 @@ class CatalogController < ApplicationController # rubocop:disable Metrics/ClassL
   include Blacklight::Catalog
   include Arclight::Catalog
 
-  # Protect searches with bot_challenge_page & turnstile
-  # See: https://github.com/samvera-labs/bot_challenge_page
-  #
-  # We protect requests for searches, but not for show pages, so we can still
-  # crawl ourselves and let well-behaved search engines index our content via
-  # the sitemap.
-  before_action only: :index do |controller|
-    BotChallengePage::BotChallengePageController.bot_challenge_enforce_filter(controller, immediate: true)
-  end
+  bot_challenge only: [:index, :facet], if: -> { has_search_parameters? }
 
   configure_blacklight do |config|
     config.logo_link = 'https://library.stanford.edu/'
--- config/initializers/bot_challenge_page.rb
+++ config/initializers/bot_challenge_page.rb
@@ -1,37 +1,44 @@
 # frozen_string_literal: true
 
-# CIDR blocks treated as local/exempt from challenges
+## CIDR blocks treated as local/exempt from challenges
 SAFELIST = [
   '171.64.0.0/14',
   '10.0.0.0/8',
   '172.16.0.0/12'
 ].freeze
 
-# rubocop:disable Layout/LineLength
-Rails.application.config.to_prepare do
+BotChallengePage.configure do |config|
   # If disabled, no challenges will be issued
-  BotChallengePage::BotChallengePageController.bot_challenge_config.enabled = ENV.fetch('TURNSTILE_ENABLED', 'false').downcase == 'true'
+  config.enabled = ENV.fetch('TURNSTILE_ENABLED', 'false').downcase == 'true'
 
   # Get from CloudFlare Turnstile: https://www.cloudflare.com/application-services/products/turnstile/
   # Some testing keys are also available: https://developers.cloudflare.com/turnstile/troubleshooting/testing/
-  BotChallengePage::BotChallengePageController.bot_challenge_config.cf_turnstile_sitekey = ENV.fetch('TURNSTILE_SITE_KEY', nil)
-  BotChallengePage::BotChallengePageController.bot_challenge_config.cf_turnstile_secret_key = ENV.fetch('TURNSTILE_SECRET_KEY', nil)
-
-  # Do the challenge "in place" on the page the user was on
-  BotChallengePage::BotChallengePageController.bot_challenge_config.redirect_for_challenge = false
+  #
+  # This set of keys will always pass the challenge; the link above includes
+  # sets that will always challenge or always fail, which is useful for local testing
+  config.cf_turnstile_sitekey = ENV.fetch('TURNSTILE_SITE_KEY', nil)
+  config.cf_turnstile_secret_key = ENV.fetch('TURNSTILE_SECRET_KEY', nil)
 
   # How long will a challenge success exempt a session from further challenges?
   # BotChallengePage::BotChallengePageController.bot_challenge_config.session_passed_good_for = 36.hours
 
   # Exempt async JS facet requests from the challenge. Someone really determined could fake
   # this header, but until we see that behavior, we'll allow it so the facet UI works.
-  # Also exempt any IPs contained in the CIDR blocks in SAFELIST.
-  BotChallengePage::BotChallengePageController.bot_challenge_config.allow_exempt = lambda do |controller, _config|
-    (controller.is_a?(CatalogController) && controller.params[:action].in?(%w[facet]) && controller.request.headers['sec-fetch-dest'] == 'empty') ||
+  # We also have an exception for index json so that the mini-bento frontend fetch in Searchworks doesn't get blocked.
+  # Also exempt any IPs contained in the CIDR blocks in Settings.turnstile.safelist.
+  # Also exempt any user agents in Settings.turnstile.allowed_user_agents (for registered bots/crawlers).
+  # Also exempt any logged-in users.
+  config.skip_when = lambda do |_config|
+    (is_a?(CatalogController) &&
+      params[:action].in?(%w[facet index]) && request.format.json? && request.headers['sec-fetch-dest'] == 'empty') ||
       SAFELIST.map { |cidr| IPAddr.new(cidr) }.any? { |range| controller.request.remote_ip.in?(range) }
   end
+
+  # Use a 200 OK status when rendering the challenge so the in-place JS challenge can work
+  config.challenge_renderer = lambda {
+    render "bot_challenge_page/bot_challenge_page/challenge", status: :ok
+  }
 
   # More configuration is available; see:
   # https://github.com/samvera-labs/bot_challenge_page/blob/main/app/models/bot_challenge_page/config.rb
 end
-# rubocop:enable Layout/LineLength
