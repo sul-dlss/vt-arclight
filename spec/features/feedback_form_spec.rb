b3217ee504dcce79dbcc8c529fa1aab58505dfd9
Cory Lown <corylown@stanford.edu>
2026-06-29 08:06:52 -0400

    Rubocop fixup

--- spec/features/feedback_form_spec.rb
+++ spec/features/feedback_form_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Feedback form', :js do
   it 'is visible only after clicking the feedback link' do
     visit root_path
 
+    expect(page).to have_link('Feedback')
     expect(page).to have_no_css('#feedback')
 
     click_on 'Feedback'
